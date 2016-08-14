39ffb551ccedd8c5e5540cdabf54a796a4b730e7
Victor Follet <follet.victor@gmail.com>
2016-08-14 14:27:29 +0300

    Calculator completed

---
+++ calculator/src/main/scala/calculator/Calculator.scala
@@ -0,0 +1,49 @@
+package calculator
+
+import java.util
+
+sealed abstract class Expr
+final case class Literal(v: Double) extends Expr
+final case class Ref(name: String) extends Expr
+final case class Plus(a: Expr, b: Expr) extends Expr
+final case class Minus(a: Expr, b: Expr) extends Expr
+final case class Times(a: Expr, b: Expr) extends Expr
+final case class Divide(a: Expr, b: Expr) extends Expr
+
+object Calculator {
+  def computeValues(
+  namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
+    namedExpressions.mapValues( (sig: Signal[Expr]) => {
+      Signal(eval(sig(), namedExpressions))
+    })
+  }
+
+  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
+    def _eval(expr: Expr, refs: Map[String, Signal[Expr]], refsStack: Set[String]): Double = {
+      expr match {
+        case Literal(v) => v
+        case Plus(a, b) => _eval(a, refs, refsStack) + _eval(b, refs, refsStack)
+        case Minus(a, b) => _eval(a, refs, refsStack) - _eval(b, refs, refsStack)
+        case Times(a, b) => _eval(a, refs, refsStack) * _eval(b, refs, refsStack)
+        case Divide(a, b) => _eval(a, refs, refsStack) / _eval(b, refs, refsStack)
+        case Ref(name) => {
+          if ((refsStack contains name) || ! (refs contains name)) Double.NaN
+          else  _eval(refs(name)(), refs, refsStack + name)
+        }
+      }
+    }
+    _eval(expr, references, Set())
+  }
+
+  /** Get the Expr for a referenced variables.
+   *  If the variable is not known, returns a literal NaN.
+   */
+  private def getReferenceExpr(name: String,
+      references: Map[String, Signal[Expr]]) = {
+    references.get(name).fold[Expr] {
+      Literal(Double.NaN)
+    } { exprSignal =>
+      exprSignal()
+    }
+  }
+}
---
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -0,0 +1,32 @@
+package calculator
+case class ZeroCoaficientException(smth:String)  extends Exception(smth)
+
+object Polynomial {
+  def computeDelta(a: Signal[Double], b: Signal[Double],
+                   c: Signal[Double]): Signal[Double] = {
+    if (a() == 0) {
+      throw new ZeroCoaficientException("Not square polinom")
+    }
+    else{
+      Signal(b() * b() - 4 * a() * c())
+    }
+  }
+
+  def computeSolutions(a: Signal[Double], b: Signal[Double],
+                       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+    Signal(
+      if (a() == 0) {
+        Set()
+      }
+      else if (delta() < 0){
+        Set()
+      }
+      else if (delta() == 0){
+        Set(-b()/(2*a()))
+      }
+      else {
+        Set( (-b() - Math.sqrt(delta()))/(2*a()), (-b() + Math.sqrt(delta()))/(2*a()))
+      }
+    )
+  }
+}
